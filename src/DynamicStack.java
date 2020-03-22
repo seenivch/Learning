273044622b21f2a699fa87342c860ce972b285b9
Seenivasan Chandrasekaran <seenu.chandar@gmail.com>
2020-03-22 17:19:41 +0530

    first commit

---
+++ src/DynamicStack.java
@@ -0,0 +1,53 @@
+class DynamicStack {
+	
+	int capacity = 2;
+	int stack[] = new int [5];
+	int top = 0;
+	public void push(int data) {
+		if(size()== capacity) 
+			expand();
+			stack[top] = data ;
+			top ++;
+		
+	}
+	
+	private void expand() {
+
+		int length = size();
+		int newStack[] = new int[capacity * 2];
+		System.arraycopy(stack, 0, newStack, 0, length);
+		stack = newStack;
+		capacity*=2;
+	}
+
+	public int pop() {
+		int data = 0;
+		if(isEmpty()) {
+			System.out.println("Stack is Empty");
+		} else {
+			top --;
+			data = stack[top];
+			stack[top] = 0;
+		}
+		return data;
+	}
+	
+	public void show() {
+		for(int n : stack) {
+			System.out.println(n + " ");
+		}
+	}
+	
+	public int peek() {
+		int data;
+		data = stack[top - 1];
+		return data;
+	}
+	
+	public int size() {
+		return top;
+	}
+	public boolean isEmpty() {
+			return top <=0;
+	}
+}
---
+++ src/StackImplementation.java
@@ -0,0 +1,16 @@
+ 
+
+class StackImplementation{
+	public static void main(String[] args) {
+		DynamicStack st = new DynamicStack();
+		st.push(10);
+		st.push(9);
+		st.push(8);
+		st.push(6);
+		st.push(5);
+		st.push(4);
+		st.show();
+////		System.out.println(st.peek());
+//		System.out.println(st.pop());
+	}
+}
